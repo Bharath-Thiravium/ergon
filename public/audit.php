3792d8f7cb17a75ea2cda02b8b8d8b0365d25938
Bharath-Thiravium <bharath@athenas.co.in>
2025-10-26 23:41:38 +0530

    Delete all files

--- public/audit.php
+++
@@ -1,5 +0,0 @@
-<?php
-require_once __DIR__ . '/../app/controllers/AuditController.php';
-$controller = new AuditController();
-$controller->index();
-?>
--- public/clear-cache.php
+++
@@ -1,5 +0,0 @@
-<?php
-header('Content-Type: text/plain');
-echo "Cache cleared - " . date('Y-m-d H:i:s') . "\n";
-echo "Please hard refresh your browser (Ctrl+F5 or Cmd+Shift+R)\n";
-?>
