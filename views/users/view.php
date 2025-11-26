c91763a8bdb9c8c6f6df2529a8f4a179ba8508bf
Bharath-Thiravium <bharath@thiravium.com>
2025-11-26 13:20:26 +0530

    `Update document links in user view to include user ID and move download link to separate route`

--- views/users/view.php
+++ views/users/view.php
@@ -109,7 +109,7 @@ ob_start();
                             </div>
                             <div class="document-actions">
                                 <div class="ab-container">
-                                    <a href="/ergon/uploads/documents/<?= urlencode($doc['filename']) ?>" 
+                                    <a href="/ergon/public/uploads/users/<?= $user['id'] ?>/<?= urlencode($doc['filename']) ?>" 
                                        class="ab-btn ab-btn--view" 
                                        target="_blank" 
                                        title="View Document">
@@ -118,9 +118,8 @@ ob_start();
                                             <path stroke-linecap="round" stroke-linejoin="round" d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z" />
                                         </svg>
                                     </a>
-                                    <a href="/ergon/uploads/documents/<?= urlencode($doc['filename']) ?>" 
+                                    <a href="/ergon/users/download-document/<?= $user['id'] ?>/<?= urlencode($doc['filename']) ?>" 
                                        class="ab-btn ab-btn--edit" 
-                                       download 
                                        title="Download Document">
                                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                             <path stroke-linecap="round" stroke-linejoin="round" d="M12 10v6m0 0l-3-3m3 3l3-3m2 8H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z" />
