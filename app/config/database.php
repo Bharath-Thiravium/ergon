86a8918f384258f90e09172901d0f7e900f414c9
Bharath-Thiravium <bharath@thiravium.com>
2026-03-20 15:12:49 +0530

    2

--- app/config/database.php
+++ app/config/database.php
@@ -15,9 +15,7 @@ if (file_exists($_envFile)) {
             list($key, $value) = explode('=', $line, 2);
             $key   = trim($key);
             $value = trim($value);
-            if (!isset($_ENV[$key])) {
-                $_ENV[$key] = $value;
-            }
+            $_ENV[$key] = $value;
         }
     }
 }
